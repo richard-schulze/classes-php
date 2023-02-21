b32996c52e4bcb2a8e0153fa484fce93703aa649
richard-schulze <richard.schulze@laplateforme.io>
2023-02-21 15:38:38 +0100

    commit gestion erreur

--- user-pdo.php
+++ user-pdo.php
@@ -8,36 +8,48 @@ class Userpdo {
     public $email;
     public $firstname;
     public $lastname;
-    public $bdd;
+    private $bdd;
 
     //création du constructeur
 public function __construct(){
     // pour la connexion à la base de donnée
     $servername = 'localhost';
     $dbname = 'classes';
-    $db_username = 'root';
-    $db_password = '';
+    $username = 'root';
+    $password = '';
 
     // on essaie la connexion
     try{
-        $this->bdd = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8,$db_username,$db_password");
+        $this->bdd = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8",$username,$password);
 
     // gestion des erreurs de PDO sur Exception
     $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Votre connexion à la base de donnée est bonne";
-    }
-    catch(PDOException $e){
-        echo "Echec de la connexion avec la base de donnée" .$e->getMessage();
+    }catch(PDOException $e){
+        echo "Echec : " .$e->getMessage();
         exit;
-    }
-    // On vérifie la connexion
-    if (isset($_SESSION['user'])){
-        $this->id = $_SESSION['user']['id'];
-        $this->login = $_SESSION['user']['login'];
-        $this->password = $_SESSION['user']['password'];
-        $this->email = $_SESSION['user']['email'];
-        $this->firstname = $_SESSION['user']['firstname'];
-        $this->lastname = $_SESSION['user']['lastname'];
-    }
+    
 }
+    
 }
+// inscription en base de donnée
+public function register($login,$password,$email,$firstname,$lastname){
+    $nouvelUser = $this->bdd->prepare("INSERT INTO utilisateurs(login,password,email,firstname,lastname)VALUE(?,?,?,?,?)");
+    $nouvelUser->execute([$login,$password,$email,$firstname,$lastname]);
+    
+    $donneesUser = $this->bdd->prepare("SELECT * FROM utilisateurs WHERE login= ?");
+    $donneesUser->execute([$_SESSION['login']]);
+    $result = $donneesUser->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+    
+}
+
+}
+/* section de test*/
+
+//création d'un nouvel utilisateur
+$user = new Userpdo();
+
+// Test pour l'inscription
+$user->register("ric", "ric","ric","ric","ric");
+//var_dump($_SESSION['login']);
